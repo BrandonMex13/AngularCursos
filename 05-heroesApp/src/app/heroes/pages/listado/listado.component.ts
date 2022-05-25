f2758acc91b1e4ab7eeec9865b59585023f2e29c
Brandon Montoya <brandon.montoya@bancaprepa.com>
2022-05-25 12:10:03 -0600

    Listado de heroes

--- 05-heroesApp/src/app/heroes/pages/listado/listado.component.ts
+++ 05-heroesApp/src/app/heroes/pages/listado/listado.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../../services/heroes.service';
+import { Heroe } from '../../interfaces/heroes.interface';
 
 @Component({
-  selector: 'app-listado',
-  templateUrl: './listado.component.html',
-  styleUrls: ['./listado.component.css']
+    selector: 'app-listado',
+    templateUrl: './listado.component.html',
+    styleUrls: ['./listado.component.css']
 })
 export class ListadoComponent implements OnInit {
 
-  constructor() { }
+    heroes : Heroe[] = [];
 
-  ngOnInit(): void {
-  }
+    constructor(
+        private heroesService : HeroesService
+    ) { }
+
+    ngOnInit(): void {
+        this.heroesService.getHeroes().subscribe( res => {
+            this.heroes = res;
+        });
+    }
 
 }
